a56a2f5ed387c89a435b7912b2d1d167eda876bb
Zayozin <zayocomercial@gmail.com>
2024-04-24 15:17:21 -0300

    -

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function Clique() {
+    let mobileMenu = document.querySelector('.mobile-menu')
+    if (mobileMenu.classList.contains('open')) {
+        mobileMenu.classList.remove('open') 
+    } else {
+        mobileMenu.classList.add('open')
+    }
+}
